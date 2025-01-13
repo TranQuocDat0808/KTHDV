28ae5e2bce845041de08977426069eeb9b06b1f3
TranQuocDat0808 <tranquocdat11a720182021@gmail.com>
2025-01-14 00:28:20 +0700

    update BaiTH2

---
+++ supermarket/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Supermarket.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ supermarket/Models/User.cs
@@ -0,0 +1,7 @@
+﻿public class User
+{
+    public int IdUser { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+}
