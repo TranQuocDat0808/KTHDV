7c1e9d9ce1030e95fbf6e8e0705ac586872f97ab
TranQuocDat0808 <tranquocdat11a720182021@gmail.com>
2025-01-18 00:14:47 +0700

    updatebth5

--- AuthAPI/Models/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthAPI.Models
-{
-    public class RegisterRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
-}
